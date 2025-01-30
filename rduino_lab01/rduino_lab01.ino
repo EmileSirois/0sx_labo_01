7eb0019d20405ceafb5f938406054b358b689717
Émile <2005emilesgmail.com>
2025-01-30 13:33:27 -0500

    second commit

--- rduino_lab01/rduino_lab01.ino
+++ rduino_lab01/rduino_lab01.ino
@@ -1,9 +1,23 @@
+const int ledPin = 13;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  //State 01 : ON
+  digitalWrite(led, HIGH);
+  Serial.print(Allumé - 6229147);
+  delay(2000);
+  
+  //State 02 : Blink
+  for (int i = 0; i > 4; i++)
+  {
+
+
+  }
 }
